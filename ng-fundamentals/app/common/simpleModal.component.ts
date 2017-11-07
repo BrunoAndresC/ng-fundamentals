a3345021d61d5a4cac107b3c3947f60529435ae6
Owner <brunocandia@gmail.com>
2017-11-07 14:38:01 -0300

    Using the @ViewChild Decorator.

--- ng-fundamentals/app/common/simpleModal.component.ts
+++ ng-fundamentals/app/common/simpleModal.component.ts
@@ -1,9 +1,10 @@
-﻿import { Component, Input } from '@angular/core';
+﻿import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from './jQuery.service';
 
 @Component({
     selector: 'simple-modal',
     template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -12,13 +13,30 @@
                     </button>
                     <h4 class="modal-title">{{title}}</h4>
                 </div>
-                <div class="modal.body">
+                <div class="modal-body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div>
         </div>
     </div>
     `,
+    //template: `
+    //<div id="{{elementId}}" class="modal fade" tabindex="-1">
+    //    <div class="modal-dialog">
+    //        <div class="modal-content">
+    //            <div class="modal-header">
+    //                <button type="button" class="close" data-dismiss="modal">
+    //                    <span>&times;</span>
+    //                </button>
+    //                <h4 class="modal-title">{{title}}</h4>
+    //            </div>
+    //            <div class="modal.body">
+    //                <ng-content></ng-content>
+    //            </div>
+    //        </div>
+    //    </div>
+    //</div>
+    //`,
     //template: `
     //<div id="simple-modal" class="modal fade" tabindex="-1">
     //    <div class="modal-dialog">
@@ -43,4 +61,12 @@
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @ViewChild('modalcontainer') containerElement: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $: any) {
+    }
+
+    closeModal() {
+        this.$(this.containerElement.nativeElement).modal('hide');
+    }
 }

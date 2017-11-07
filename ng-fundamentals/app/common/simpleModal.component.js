f636f87fd23f7c19c3c81721b6f7eb2be6441f32
Owner <brunocandia@gmail.com>
2017-11-07 14:54:25 -0300

    Creating Settings on Components.

--- ng-fundamentals/app/common/simpleModal.component.js
+++ ng-fundamentals/app/common/simpleModal.component.js
@@ -19,7 +19,9 @@ var SimpleModalComponent = /** @class */ (function () {
         this.$ = $;
     }
     SimpleModalComponent.prototype.closeModal = function () {
-        this.$(this.containerElement.nativeElement).modal('hide');
+        if (this.closeOnBodyClick.toLocaleLowerCase() === "true") {
+            this.$(this.containerElement.nativeElement).modal('hide');
+        }
     };
     __decorate([
         core_1.Input(),
@@ -29,6 +31,10 @@ var SimpleModalComponent = /** @class */ (function () {
         core_1.Input(),
         __metadata("design:type", String)
     ], SimpleModalComponent.prototype, "elementId", void 0);
+    __decorate([
+        core_1.Input(),
+        __metadata("design:type", String)
+    ], SimpleModalComponent.prototype, "closeOnBodyClick", void 0);
     __decorate([
         core_1.ViewChild('modalcontainer'),
         __metadata("design:type", core_1.ElementRef)

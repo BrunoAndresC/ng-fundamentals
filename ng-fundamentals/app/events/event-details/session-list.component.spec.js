861bddf2ebd37c8c1324639dae932c4b013cf4b7
Owner <brunocandia@gmail.com>
2017-11-14 10:50:36 -0300

    Testing Components with Isolated Tests.

---
+++ ng-fundamentals/app/events/event-details/session-list.component.spec.js
@@ -0,0 +1,10 @@
+"use strict";
+var session_list_component_1 = require("./session-list.component");
+describe('SessionListComponent', function () {
+    var component;
+    var mockAuthService, mockVoterService;
+    beforeEach(function () {
+        component = new session_list_component_1.SessionListComponent(mockAuthService, mockAuthService);
+    });
+});
+//# sourceMappingURL=session-list.component.spec.js.map
